f54a9753b650db05714aeaa529b89f9c0327bd44
Tereza <bisharyant@gmail.com>
2024-08-16 14:48:56 +0400

    Added the terraform part

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 resource "helm_release" "argocd" {
-  name = "arggocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart = "argo-cd"
-  namespace = "argocd"
+  name             = "arggocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
   create_namespace = true
-  version = "7.3.11"
-  values = [file("argo-values.yaml")]
+  version          = "7.3.11"
+  values = [templatefile("argo-values.yaml", {
+    client_id     = 
+    client_secret = 
+  })]
 }
